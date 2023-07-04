08d9619e64dbfec713327958c16e2cc825fa686b
Chris Nelson <chrisnelson7791@gmail.com>
2023-07-04 16:27:19 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function makeNegative(num) {
+
+  return num < 0 ? num : num * -1
+  
+}
+
+
+// params: num
+// return: num    // make it negative if it is not already negative
+// e.g. makeNegative(458)
+// results in -458
+
+// if the number is less than 0 return the number
+// else return the number * -1
